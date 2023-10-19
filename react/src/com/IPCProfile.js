49ed695792a821a261ece425bf1c0d47cd233858
Ann Esther Joy <31465123+AnnEsther@users.noreply.github.com>
2023-10-19 20:41:29 +0000

    changing demo -> required

--- react/src/com/IPCProfile.js
+++ react/src/com/IPCProfile.js
@@ -22,18 +22,9 @@ import IPCInstance from '../lib/IPCInstance';
 import config from '../config';
 
 import PropTypes from 'prop-types';
-import Avatar from '@mui/material/Avatar';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
-import PersonIcon from '@mui/icons-material/Person';
-import AddIcon from '@mui/icons-material/Add';
-import Typography from '@mui/material/Typography';
-import { blue } from '@mui/material/colors';
+
+import Paper from '@mui/material/Paper';
 
 const IPCContext = IPCInstance.IPCContext;
 
@@ -180,46 +171,20 @@ function getSearchWalletListener(navigate, wallet_address) {
 const emails = ['username@gmail.com', 'user02@gmail.com'];
 
 function SimpleDialog(props) {
-  const { onClose, selectedValue, open } = props;
-
-  const handleClose = () => {
-    onClose(selectedValue);
-  };
+  const { onClose, open, fileName } = props;
+  const imgPath = "https://github.com/AnnEsther/IPC_Headshots/blob/main/Output/" + fileName + ".png?raw=true";
 
   const handleListItemClick = (value) => {
     onClose(value);
   };
 
   return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Set backup account</DialogTitle>
-      <List sx={{ pt: 0 }}>
-        {emails.map((email) => (
-          <ListItem disableGutters key={email}>
-            <ListItemButton onClick={() => handleListItemClick(email)}>
-              <ListItemAvatar>
-                <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
-                  <PersonIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={email} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-        <ListItem disableGutters>
-          <ListItemButton
-            autoFocus
-            onClick={() => handleListItemClick('addAccount')}
-          >
-            <ListItemAvatar>
-              <Avatar>
-                <AddIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Add account" />
-          </ListItemButton>
-        </ListItem>
-      </List>
+    <Dialog open={open}>
+      <Box>
+      <Paper>
+        <img src={imgPath}/>
+      </Paper>
+      </Box>
     </Dialog>
   );
 }
@@ -227,7 +192,7 @@ function SimpleDialog(props) {
 SimpleDialog.propTypes = {
   onClose: PropTypes.func.isRequired,
   open: PropTypes.bool.isRequired,
-  selectedValue: PropTypes.string.isRequired,
+  fileName: PropTypes.string.isRequired,
 };
 
 export default function IPCProfile(props) {
@@ -252,7 +217,6 @@ export default function IPCProfile(props) {
   //AI S1 button
 
   const [popupOpen, setPopupOpen] = React.useState(false);
-  const [selectedValue, setSelectedValue] = React.useState(emails[1]); //to be removed
 
   const handlePopupOpen = () => {
     setPopupOpen(true);
@@ -407,11 +371,14 @@ export default function IPCProfile(props) {
       </Tooltip>
       </ClickAwayListener>
 
-      <Button variant="outlined" onClick={handlePopupOpen}>
-        Open simple dialog
+      <Button 
+        variant="contained" 
+        onClick={handlePopupOpen}
+        sx={{margin: '8px 4px'}}>
+        IPC.AI.S1
       </Button>
       <SimpleDialog
-        selectedValue={selectedValue}
+        fileName={ipc.token_id}
         open={popupOpen}
         onClose={handlePopupClose}
       />
