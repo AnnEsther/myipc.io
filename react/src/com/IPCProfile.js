bc55cdbca8187445a4d123257836c207d2749b33
Ann Esther Joy <31465123+AnnEsther@users.noreply.github.com>
2023-10-19 19:58:19 +0000

    Adding demo dependencies

--- react/src/com/IPCProfile.js
+++ react/src/com/IPCProfile.js
@@ -21,6 +21,21 @@ import IPCWalletUI from '../lib/IPCWalletUI';
 import IPCInstance from '../lib/IPCInstance';
 import config from '../config';
 
+import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import PersonIcon from '@mui/icons-material/Person';
+import AddIcon from '@mui/icons-material/Add';
+import Typography from '@mui/material/Typography';
+import { blue } from '@mui/material/colors';
+
 const IPCContext = IPCInstance.IPCContext;
 
 function IPCSprite(props) {
